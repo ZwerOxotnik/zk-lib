4d85153066f88e6d13e7abb19999ae3c608d11c1
ZwerOxotnik <zweroxotnik@gmail.com>
2020-07-30 16:49:26 +0300

    Update

--- data.lua
+++ data.lua
@@ -1,6 +1,2 @@
 require("data-api/puan_api")
-
-local sounds_list = require("sound/sounds_list")
-sounds_list.name = "zk-lib" -- to add in programmable speakers 
-sounds_list.path = "__zk-lib__/sound/"
-puan_api.add_sounds(sounds_list)
+require("sound/sounds_list")
--- sound/sounds_list.lua
+++ sound/sounds_list.lua
@@ -1,5 +1,9 @@
--- Please, do not change this file! More info: https://www.reddit.com/r/ZwerOxotnik/comments/dv7tpx/you_want_your_own_mod_stay_tuned/ 
-return { 
+-- Please, do not change this file if you're not sure, except sounds_list.name!
+-- More info: https://www.reddit.com/r/ZwerOxotnik/comments/dv7tpx/you_want_your_own_mod_stay_tuned/
+-- You need require this file to your control.lua and add https://mods.factorio.com/mod/zk-lib in your dependencies
+local sounds_list = { 
+	name = "zk-lib", --change me, if you want add these sounds to programmable speakers
+	path = "__zk-lib__/sound/", -- path to this folder
 	sounds = { 
 		{ 
 			name = "flashlight-click", 
@@ -10,7 +14,7 @@ return {
 		{ 
 			name = "infobleep", 
 		}, 
-		{ 
+		{
 			name = "interface3", 
 		}, 
 		{ 
@@ -18,3 +22,5 @@ return {
 		}, 
 	} 
 } 
+if puan_api then puan_api.add_sounds(sounds_list) end 
+return sounds_list 
