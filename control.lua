a7f797653ac86a458d6d4895bb2eec5536f0ae76
ZwerOxotnik <zweroxotnik@gmail.com>
2020-07-31 09:26:34 +0300

    Disable zk_commands

--- control.lua
+++ control.lua
@@ -4,6 +4,6 @@ random_items = require("static-libs/lualibs/random_items")
 local modules = {}
 modules.random_items = random_items
 modules.zk_lib = require("zk-lib/control")
-modules.zk_commands = require("zk-lib/zk_commands")
+-- modules.zk_commands = require("zk-lib/zk_commands")
 
 event_listener.add_libraries(modules)
