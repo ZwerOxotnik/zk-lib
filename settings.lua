47219bca7a798cf34a033ba9427bf7dfb1896ad2
ZwerOxotnik <zweroxotnik@gmail.com>
2020-08-01 10:19:24 +0300

    Update settings

--- settings.lua
+++ settings.lua
@@ -1,10 +1,10 @@
 data:extend({
-	{
-		type = "bool-setting",
-		name = "zk_commands",
-		setting_type = "runtime-global",
-		default_value = true
-	},
+	-- {
+	-- 	type = "bool-setting",
+	-- 	name = "zk_commands",
+	-- 	setting_type = "runtime-global",
+	-- 	default_value = true
+	-- },
 	{
 		type = "bool-setting",
 		name = "EL_debug-mode",
