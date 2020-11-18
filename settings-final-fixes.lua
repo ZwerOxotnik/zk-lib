96195428073e7dcc59592f16c773708c93e2b5c3
ZwerOxotnik <zweroxotnik@gmail.com>
2020-11-18 12:27:27 +0300

    Add an additional check

--- settings-final-fixes.lua
+++ settings-final-fixes.lua
@@ -2,14 +2,16 @@ local addons_list = require("addons/addons-list")
 
 addons_settings = {}
 for _, name in pairs(addons_list) do
-  table.insert(addons_settings, {
-    type = "string-setting",
-    name = "zk-lib-during-game_" .. name,
-    setting_type = "runtime-global",
-    default_value = "enabled",
-    allowed_values = {"disabled", "enabled"},
-    localised_name = {"mod-name." .. name},
-    localised_description = {"mod-description." .. name}
-  })
+  if data.raw["string-setting"]["zk-lib_" .. name] then
+    table.insert(addons_settings, {
+      type = "string-setting",
+      name = "zk-lib-during-game_" .. name,
+      setting_type = "runtime-global",
+      default_value = "enabled",
+      allowed_values = {"disabled", "enabled"},
+      localised_name = {"mod-name." .. name},
+      localised_description = {"mod-description." .. name}
+    })
+  end
 end
 data:extend(addons_settings)
