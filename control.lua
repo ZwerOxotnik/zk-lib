20ba1566ddaf38c5267d460db0093db812bfa907
ZwerOxotnik <zweroxotnik@gmail.com>
2020-11-20 13:49:18 +0300

    Update to 0.3.5

--- control.lua
+++ control.lua
@@ -42,37 +42,36 @@ for name, addon_data in pairs(addons_list) do
       table.insert(modules, {
         events = {[defines.events.on_runtime_mod_setting_changed] = function(event)
           if event.setting_type ~= "runtime-global" then return end
+          if event.setting ~= "zk-lib-during-game_" .. name then return end
 
-          if event.setting == "zk-lib-during-game_" .. name then
-            if settings.global[event.setting].value == true then
-              if module.add_commands and module.remove_commands then module.add_commands() end
-              if module.add_remote_interface and module.remove_remote_interface then module.add_remote_interface() end
-              module.events = module.get_default_events()
-              if module.enabled then module.enabled() end
-              if module.update_glolbal_data then
-                module.update_glolbal_data()
-              elseif module.on_init then
-                module.on_init()
-              end
-              game.print({"", {"gui-mod-info.status-enabled"}, ": ", {"mod-name." .. name}})
-            else
-              if module.add_commands and module.remove_commands then module.remove_commands() end
-              if module.add_remote_interface and module.remove_remote_interface then module.remove_remote_interface() end
-              module.check_events()
-              if module.disabled then module.disabled() end
-              game.print({"", {"gui-mod-info.status-disabled"}, ": ", {"mod-name." .. name}})
+          if settings.global[event.setting].value == true then
+            if module.add_commands and module.remove_commands then module.add_commands() end
+            if module.add_remote_interface and module.remove_remote_interface then module.add_remote_interface() end
+            module.events = module.get_default_events()
+            if module.enabled then module.enabled() end
+            if module.update_global_data then -- it's a workaround to init global data because those addons don't init sometimes
+              module.update_global_data()
+            elseif module.on_init then
+              module.on_init()
             end
-            if module.events then
-              for id, _ in pairs(module.events) do
-                if module.blacklist_events[id] ~= true and id ~= "lib_id" then
-                  event_listener.update_event(module, id)
-                end
+            game.print({"", {"gui-mod-info.status-enabled"}, ": ", {"mod-name." .. name}})
+          else
+            if module.add_commands and module.remove_commands then module.remove_commands() end
+            if module.add_remote_interface and module.remove_remote_interface then module.remove_remote_interface() end
+            module.check_events()
+            if module.disabled then module.disabled() end
+            game.print({"", {"gui-mod-info.status-disabled"}, ": ", {"mod-name." .. name}})
+          end
+          if module.events then
+            for id, _ in pairs(module.events) do
+              if module.blacklist_events[id] ~= true and id ~= "lib_id" then
+                event_listener.update_event(module, id)
               end
             end
-            if module.on_nth_tick and #module.on_nth_tick > 0 then
-              for tick, _ in pairs(module.events) do
-                event_listener.update_nth_tick(module, tick)
-              end
+          end
+          if module.on_nth_tick and #module.on_nth_tick > 0 then
+            for tick, _ in pairs(module.events) do
+              event_listener.update_nth_tick(module, tick)
             end
           end
         end}}
--- core/control.lua
+++ core/control.lua
@@ -1,31 +1,52 @@
 local zk_lib = {}
 remote.remove_interface('zk-lib')
 remote.add_interface('zk-lib', {
-    insert_random_item = random_items.insert_random_item,
-    transfer_items = LuaEntity.transfer_items
+	insert_random_item = random_items.insert_random_item,
+	transfer_items = LuaEntity.transfer_items
 })
 
-local function set_global_data()
-    global.zk_lib = global.zk_lib or {}
-    global.zk_lib.save_tick = global.zk_lib.save_tick or 0
+local addons_list = require("addons/addons-list")
+local function on_init()
+	global.zk_lib = global.zk_lib or {}
+	global.zk_lib.save_tick = global.zk_lib.save_tick or 0
+
+	for addon_name, _ in pairs(addons_list) do
+		if settings.global["zk-lib-during-game_" .. addon_name]
+			and settings.startup["zk-lib_" .. addon_name]
+			and settings.startup["zk-lib_" .. addon_name].value ~= "mutable" then
+			if settings.global["zk-lib-during-game_" .. addon_name].value == true then
+				settings.global["zk-lib-during-game_" .. addon_name] = {value = false}
+			end
+		end
+	end
 end
 
-zk_lib.on_init = set_global_data
+zk_lib.on_init = on_init
 zk_lib.on_configuration_changed = set_global_data
 
--- if safe mode is enabled then save game as an admin change state of a mutable addon
 local function on_runtime_mod_setting_changed(event)
-    if event.setting_type ~= "runtime-global" then return end
-    if settings.global["zk-lib_safe-mode"].value == false then return end
-    if game.tick < global.zk_lib.save_tick then return end
+	if event.setting_type ~= "runtime-global" then return end
+	if string.find(event.setting, "^zk.lib.during.game_") ~= 1 then return end
 
-    if string.match(event.setting, "^zk.lib.during.game_") then
-        global.zk_lib.save_tick = game.tick + 4800
-        game.auto_save()
+
+	local addon_name = string.gsub(event.setting, "^zk.lib.during.game_", "")
+	if not addon_name then return end
+
+	if settings.startup["zk-lib_" .. addon_name].value == "mutable" then
+		-- if safe mode is enabled then save game as an admin change state of a mutable addon
+		if settings.global["zk-lib_safe-mode"].value == false then return end
+		if game.tick < global.zk_lib.save_tick then return end
+
+		global.zk_lib.save_tick = game.tick + 4800
+		game.auto_save()
+	else
+		if settings.global["zk-lib-during-game_" .. addon_name].value == true then
+			settings.global["zk-lib-during-game_" .. addon_name] = {value = false}
+		end
 	end
 end
 
 zk_lib.events = {
-    [defines.events.on_runtime_mod_setting_changed] = on_runtime_mod_setting_changed
+	[defines.events.on_runtime_mod_setting_changed] = on_runtime_mod_setting_changed
 }
 return zk_lib
