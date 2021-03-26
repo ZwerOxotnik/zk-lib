c6f9687358e281f62eb90c3942eeae54cabaabc1
ZwerOxotnik <zweroxotnik@gmail.com>
2021-03-26 11:15:49 +0300

    Update dates

--- addons/teams-zo.lua
+++ addons/teams-zo.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2019-2020 ZwerOxotnik <zweroxotnik@gmail.com>
+Copyright 2019-2021 ZwerOxotnik <zweroxotnik@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
