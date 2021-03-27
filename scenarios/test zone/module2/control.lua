7167c7c5ecec34287d0d5b1a66c2c13717530987
ZwerOxotnik <zweroxotnik@gmail.com>
2021-03-27 09:47:35 +0300

    Change  example

--- scenarios/test zone/module2/control.lua
+++ scenarios/test zone/module2/control.lua
@@ -2,36 +2,12 @@ local module = {}
 module.events = {}
 module.version = "1.0.0"
 
-local function get_event(event)
-	if type(event) == "number" then
-		return event
-	else
-		return defines.events[event] --or event
-	end
-end
-
--- This function for compatibility with "Event listener" module and into other modules
-local function put_event(event, func)
-	event = get_event(event)
-	if event then
-		module.events[event] = func
-		if Event then
-			Event.register(event, func)
-		end
-		return true
-	else
-		log("The event is nil")
-		-- error("The event is nil")
-	end
-	return false
-end
-
 -- For tests
 local function on_player_joined_game()
 	game.print('2nd test of event listener')
 end
 
 -- For attaching events
-put_event("on_player_joined_game", on_player_joined_game)
+module.events[defines.events.on_player_joined_game] = on_player_joined_game
 
 return module
