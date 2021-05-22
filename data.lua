18179db2d91d7084fc61dd33596f0e55f17e8af2
ZwerOxotnik <zweroxotnik@gmail.com>
2021-05-22 12:42:10 +0300

    Add speech-bubble-no-fade

--- data.lua
+++ data.lua
@@ -10,3 +10,16 @@ zk_lib.attach_custom_input_event("move-right")
 zk_lib.attach_custom_input_event("move-up")
 zk_lib.attach_custom_input_event("mine")
 zk_lib.attach_custom_input_event("toggle-map")
+
+if data.raw["speech-bubble"]["speech-bubble-no-fade"] == nil then
+  data:extend({
+    {
+      type = "speech-bubble",
+      name = "speech-bubble-no-fade",
+      style = "compilatron_speech_bubble",
+      wrapper_flow_style = "compilatron_speech_bubble_wrapper",
+      fade_in_out_ticks = 5,
+      flags = {"not-on-map", "placeable-off-grid"}
+    }
+  })
+end
