4608c494075b6df2c1233419938a9d431f347d2e
ZwerOxotnik <zweroxotnik@gmail.com>
2021-09-13 10:24:21 +0300

    Update to 0.9.7

--- settings.lua
+++ settings.lua
@@ -57,23 +57,6 @@ for name, addon in pairs(insecure_addons_list) do
 		}
 	})
 end
-data:extend(addons_settings)
-
-local mods_settings = {}
-for name in pairs(mods) do
-	local is_ok = pcall(require, "__" .. name .. "__/switchable_mod")
-	if is_ok then
-		local setting_name = "mod_" .. name
-		table.insert(mods_settings, {
-			type = "bool-setting",
-			name = setting_name,
-			order = setting_name,
-			setting_type = "runtime-global",
-			default_value = true,
-			localised_name = {"", "[color=orange]", "Mod", {"colon"}, "[/color] ", {"mod-name." .. name}},
-		})
-	end
-end
-if #mods_settings > 0 then
-	data:extend(mods_settings)
+if #addons_settings > 0 then
+	data:extend(addons_settings)
 end
