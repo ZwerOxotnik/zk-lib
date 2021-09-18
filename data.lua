1e548e5cc36112f406cee9bf37c53516183e33aa
ZwerOxotnik <zweroxotnik@gmail.com>
2021-09-18 11:58:02 +0300

    Update to 0.9.9

--- data.lua
+++ data.lua
@@ -4,10 +4,8 @@ zk_lib = require("data-api/zk_lib")
 require("sound/sounds_list")
 
 
-local MOD_PATH = "__zk-lib__"
-
-
 require("prototypes/utility-sprites")
+require("prototypes/styles")
 
 
 zk_lib.attach_custom_input_event("move-down")
@@ -20,7 +18,7 @@ zk_lib.attach_custom_input_event("toggle-map")
 -- TODO: create special scripts
 -- zk_lib.create_tool({
 --   name = "zk-select", -- this is fake cursor to find cursor position via https://lua-api.factorio.com/latest/events.html#on_script_trigger_effect
---   icon = MOD_PATH .. "/graphics/select.png", --"__core__/graphics/mouse-cursor.png",
+--   icon = "__zk-lib__/graphics/select.png", --"__core__/graphics/mouse-cursor.png",
 --   radius_color = {0, 0, 0, 0}
 -- })
 
---
+++ prototypes/styles.lua
@@ -0,0 +1,8 @@
+local styles = data.raw["gui-style"].default
+
+styles.zk_action_button_dark = {
+  type = "button_style",
+  parent = "slot_button",
+	default_font_color = {1, 1, 1},
+	height = 30
+}
