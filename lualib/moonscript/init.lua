56f588747ea559d0cdd65c0dda45b406531937ba
ZwerOxotnik <zweroxotnik@gmail.com>
2022-08-29 12:07:42 +0300

    Add "moonscript" library

---
+++ lualib/moonscript/init.lua
@@ -0,0 +1,5 @@
+do
+  local _with_0 = require("__zk-lib__/lualib/moonscript/base")
+  _with_0.insert_loader()
+  return _with_0
+end
---
+++ lualib/moonscript/transform.lua
@@ -0,0 +1,4 @@
+return {
+  Statement = require("__zk-lib__/lualib/moonscript/transform/statement"),
+  Value = require("__zk-lib__/lualib/moonscript/transform/value")
+}
