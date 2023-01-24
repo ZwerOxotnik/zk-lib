2d31e8b3dab6a53d4a9e11421e9c0cd5d582b0cd
ZwerOxotnik <zweroxotnik@gmail.com>
2023-01-24 14:16:23 +0300

    Add version to lal

--- lualib/lal/lal.lua
+++ lualib/lal/lal.lua
@@ -25,7 +25,7 @@ local Parser   = require '__zk-lib__/lualib/lal/lang/parser'
 local Compiler = require '__zk-lib__/lualib/lal/lang/compiler'
 local util     = require '__zk-lib__/lualib/lal/lang/util'
 
-local lal = {}
+local lal = {VERSION = "05.20.2017" }
 
 function lal.eval(lal_code, input_file)
     local lua_code = Compiler.compile_lal_code(lal_code, nil, input_file)
